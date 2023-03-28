5b00668aa3436c198ac0f321df095045f2b0b09f
Milton Jesus <milton.lima@outlook.com>
2023-03-28 18:11:07 -0300

    added short variables

---
+++ learning-path/day-03/main_short_variable.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	name:= "Milton"
+	name = "Lima"
+	fmt.Println(name)
+}
---
+++ learning-path/day-03/main_short_variable_one.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	numero1 := 9
+	numero2 := 6
+	total := (numero1 * numero2)
+	fmt.Println(total)
+}
