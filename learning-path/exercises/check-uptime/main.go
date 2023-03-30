54d3b6eafb70d85ad566ee3f483ed99054f035b4
Milton Jesus <milton.lima@outlook.com>
2023-03-30 19:53:14 -0300

    add check url program

--- learning-path/exercises/check-uptime/main.go
+++ learning-path/exercises/check-uptime/main.go
@@ -1,10 +1,53 @@
 package main
 
-import "github.com/"
-import "fmt"
+import (
+    "fmt"
+    "net/http"
+    "time"
+	"os"
+	"bufio"
+)
+
+func checkURL(url string) (int64, error) {
+    startTime := time.Now()
+	client := http.Client{
+		Timeout: 5 * time.Second,
+	}
+    resp, err := http.Get(url)
+	if err != nil {
+		return 0, err
+	}
+	defer resp.Body.Close()
+
+	return time.Since(startTime).Nanoseconds(), nil
+}
 
 func main() {
+	file, err := os.Open("urls.txt")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer file.Close()
 
-	fmt.Scanf("Put url: %s", url)
-	
+	var wg sync.WaitGroup
+
+	scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+		wg.Add(1)
+		go func(url string) {
+			defer wg.Done()
+			latency, err := checkURL(url)
+			if err != nil {
+				fmt.Printf("%s is down with error: %v\n", url, err)
+				return
+        	}
+        	fmt.Printf("%s is up with latency: %d nanoseconds\n", url, latency)
+    	}(scanner.Text())
+	}
+	wg.Wait()
+    if scanner.Err() != nil {
+        fmt.Println(scanner.Err())
+        os.Exit(1)
+    }
 }
