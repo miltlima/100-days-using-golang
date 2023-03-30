cb2e13b56adccaf51aadeac994ee5308d00ca26f
Milton Jesus <milton.lima@outlook.com>
2023-03-29 21:15:58 -0300

    add reflect with vars

---
+++ learning-path/day-04/main_using_reflect_with_vars.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+import "reflect"
+
+
+func main() {
+	var grades int = 42
+	var message string = "hello world"
+
+	fmt.Printf("variable grades=%v is of type %v \n", grades, reflect.TypeOf(grades))
+	fmt.Printf("variable message='%v' is of type %v \n", message, reflect.TypeOf(message))
+
+}
