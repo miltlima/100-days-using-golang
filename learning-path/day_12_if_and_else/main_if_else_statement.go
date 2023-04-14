8c25963e5b5eaabfbceb66737861d87d57fcbcad
Milton Jesus <milton.lima@outlook.com>
2023-04-14 19:43:40 -0300

    add day 12 folder and examples if-else

---
+++ learning-path/day_12_if_and_else/main_if_else_statement.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var fruit string = "grapes"
+	if fruit == "apples" {
+		fmt.Println("Fruit is a apple")
+	} else {
+		fmt.Println("Fruit is not apple")
+	}
+}
