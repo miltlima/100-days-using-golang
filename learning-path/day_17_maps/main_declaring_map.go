7b7fc25b9dbaa903258d03513787bc0b0e7b91a0
Milton Jesus <milton.lima@outlook.com>
2023-04-20 19:34:08 -0300

    add map examples

---
+++ learning-path/day_17_maps/main_declaring_map.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	codes := map[string]string{"en": "English", "es": "Spanish"}
+
+	fmt.Println(codes)
+}
---
+++ learning-path/day_17_maps/main_length_map.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	codes := map[string]string{"en": "English", "fr": "French", "hi": "Hindi"}
+
+	fmt.Println(len(codes))
+}
