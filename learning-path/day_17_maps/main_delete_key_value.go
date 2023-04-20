f226c3a3fe79a9f82f5c1d04a0711bc724d4b9a0
Milton Jesus <milton.lima@outlook.com>
2023-04-20 20:24:58 -0300

    add some examples

---
+++ learning-path/day_17_maps/main_delete_key_value.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	codes := map[string]string{"en": "English", "fr": "French", "hi": "Hindi"}
+	codes["it"] = "Italian"
+
+	fmt.Println(codes)
+	delete(codes, "en")
+	fmt.Println(codes)
+
+}
