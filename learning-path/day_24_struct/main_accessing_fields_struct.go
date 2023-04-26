225100686d3341167927b47b702d25fd174f280b
Milton Jesus <milton.lima@outlook.com>
2023-04-25 23:38:36 -0300

    add more structs examples

---
+++ learning-path/day_24_struct/main_accessing_fields_struct.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+
+type Circle struct {
+	x int
+	y int
+	radius int
+}
+
+func main() {
+	var c Circle
+	c.x = 5
+	c.y = 5
+	c.radius = 5
+	fmt.Printf("%+v \n", c)
+	fmt.Printf("%+v \n", c.area) // cant access this method because is not defined
+}
